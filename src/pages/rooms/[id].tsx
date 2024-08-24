970e338d72ed72ee8abe9437a4e8f5bd10419c6a
sotasage <sotasota.0702@gmail.com>
2024-08-25 02:43:56 +0900

    退室ボタンを押したときに前のページに戻るように修正

--- src/pages/rooms/[id].tsx
+++ src/pages/rooms/[id].tsx
@@ -32,7 +32,7 @@ export default function Home() {
         <Header />
         <div className="absolute right-1 top-1 flex ">
           <button
-            onClick={() => {router.push("/")}}
+            onClick={() => {router.back()}}
             className="px-4 py-2 bg-green-500 text-white text-2xl rounded hover:bg-green-600"
           >
             退室
